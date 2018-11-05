b9f0101fcf68add88b8e76f8f6e6bcebddd9c9dc
bgabrielma <b.gabriel.ma@gmail.com>
2018-11-05 23:59:06 +0000

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(31, 92);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(648, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(141, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(207, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Test Async";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_ClickAsync);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(53, 141);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(601, 96);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(365, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(703, 314);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        /**
+         * Global variables 
+         ***/
+        List<string> links = new List<string>();
+        string x = string.Empty;
+
+        /**
+         * Token to identify an task
+         ***/
+        CancellationTokenSource cts;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            links.Add("https://www.yahoo.com");
+            links.Add("https://www.google.com");
+            links.Add("https://www.microsoft.com");
+            links.Add("https://www.cnn.com");
+            links.Add("https://www.stackoverflow.com");
+            links.Add("https://www.codeproject.com");
+            links.Add("https://www.facebook.com");
+            links.Add("https://www.policiarcc.com");
+        }
+
+        public void resetConfig()
+        {
+            progressBar1.Value = 0;
+            richTextBox1.Text = "";
+        }
+
+        /*
+         * ATENÇÃO - QUALQUER EVENTO de botão deverá ser VOID! não TASK! 
+         **/
+
+        private async void button1_ClickAsync(object sender, EventArgs e)
+        {
+            try
+            {
+
+                /**
+                 * Instance cts variable. This is required to "restart" or "recreate" the task
+                 ***/
+                cts = new CancellationTokenSource();
+
+                /**
+                 * Reset progress bar and clear textbox resut
+                 ***/
+                resetConfig();
+
+                /**
+                 * Clock to count this operation. 
+                 ***/
+                var watch = Stopwatch.StartNew();
+
+                /**
+                 * Begin process. Use await do asyn functions. 
+                 * Em PT: await runDownload. O programa espera estas funções em background serem resolvidas/processadas
+                 ***/
+                await runDownloadSync(cts.Token); // Em PT: envio um parâmetro "cts" - já definida para identificar a task.
+                watch.Stop();
+                MessageBox.Show($"Tempo decorrido: { watch.ElapsedMilliseconds / 1000 } segundos");
+            }
+            catch(OperationCanceledException ex) /* C# Tasks cancelation event */
+            {
+                resetConfig();
+                richTextBox1.Text = $" - Esta operação foi cancelada!";
+                button1.Enabled = true;
+            }
+        }
+
+        private async Task runDownloadSync(CancellationToken cancellationToken)
+        {
+            button1.Enabled = false;
+            foreach(string url in links)
+            {
+                // Em PT: Aqui é o segredo do cancelamento. Eu "atiro" ao sistema um aviso para cancelar a task de acordo com o meu parâmetro.
+                cancellationToken.ThrowIfCancellationRequested();
+
+                var value = progressBar1.Value;
+                /**
+                * Método task - corre as funções em background
+                ***/
+                await Task.Run(() => DownloadWebsite(url));
+
+                richTextBox1.Text += url + "\n";
+                progressBar1.Value = (value + 20 > 100) ? progressBar1.Value = 100 : progressBar1.Value += 20;
+            }
+            button1.Enabled = true;
+        }
+
+        private void DownloadWebsite(string url)
+        {
+            WebClient client = new WebClient();
+            client.DownloadString(url);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cts.Cancel();
+        }
+    }
+}
